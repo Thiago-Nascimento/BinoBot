0fc1bb6c6e2bb8219b912b3b77376458a4ea9c7c
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2020-06-13 18:02:19 -0300

    First Commit

---
+++ CalculaFrete/index.js
@@ -0,0 +1,31 @@
+const AWS = require('aws-sdk');
+
+exports.handler = async (event, context) => {
+    let body;
+    let statusCode = '200';
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    try {
+        switch (event.httpMethod) {
+            case 'POST':
+                console.log("Recebeu um post");
+                console.log(event.body);
+                break;
+            default:
+                throw new Error(`Unsupported method "${event.httpMethod}"`);
+        }
+    } catch (err) {
+        statusCode = '400';
+        body = err.message;
+    } finally {
+        body = JSON.stringify(body);
+    }
+
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
